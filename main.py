e6e8a3dfcf243da2d336b4e9bd68ba3b0673496f
Prakriti Pradhan <pradhanprakrity509@gmail.com>
2026-06-01 22:56:43 +0545

    discription

---
+++ main.py
@@ -0,0 +1,28 @@
+from read import read_products
+from operation import process_sale, restock, display_products
+
+def main():
+    products = read_products()
+
+    while True:
+        print("\n--- WeCare Product Sales System ---")
+        print("1. Display Products")
+        print("2. Process Sale")
+        print("3. Restock Products")
+        print("4. Exit")
+
+        choice = input("Choose an option: ")
+        if choice == "1":
+            display_products(products)
+        elif choice == "2":
+            process_sale(products)
+        elif choice == "3":
+            restock(products)
+        elif choice == "4":
+            print("Exiting system.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
